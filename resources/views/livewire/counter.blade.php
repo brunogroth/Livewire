ed6b3ec5c10c01758df17cfb04f2acfac0c1b2e9
brunogroth <brunom.groth@gmail.com>
2023-05-02 20:21:44 -0300

    feat: project #1 - Counter | Add TailwindCSS

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div class="p-16 flex justify-center gap-6 items-center">
+    <button wire:click="increment" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">+</button>
+    <span class="py-2 px-4 ">{{$count}}</span>
+    <button wire:click="decrement" class="py-2 px-4 bg-indigo-500 hover:bg-indigo-600 rounded text-white">-</button>
+</div>
