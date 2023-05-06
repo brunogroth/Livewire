066476d1481dd265e16910146e361d98c2c38f18
brunogroth <brunom.groth@gmail.com>
2023-05-06 01:05:23 -0300

    Feat: Project #5 - Product List & Search

--- app/Http/Livewire/ProductSearch.php
+++ app/Http/Livewire/ProductSearch.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ProductSearch extends Component
 {
+    use WithPagination;
+
+    public string $search = "";
+
+    protected $queryString = ['search'];
     public function render()
     {
-        return view('livewire.product-search');
+        $query = Product::query();
+        if ($this->search) {
+            $query->where('title', 'like', "%{$this->search}%")
+                ->orWhere('description', 'like', "%{$this->search}%");
+        }
+        return view('livewire.product-search', [
+            'products' => $query->paginate(8)
+        ]);
+    }
+
+    public function updated($property)
+    {
+        if ($property === 'search') {
+            $this->resetPage();
+        }
     }
 }
--- resources/views/livewire/product-search.blade.php
+++ resources/views/livewire/product-search.blade.php
@@ -1,3 +1,25 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="container mx-auto py-16 px-8">
+    <div class="mb-4">
+        <input type="text" wire:model.lazy="search" placeholder="Search for Products">
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        @foreach($products as $product)
+            <div class="max-w-xs bg-white rounded-lg overflow-hidden drop-shadow-lg drop-shadow-2xla">
+                <img class="h-48 w-full object-cover" src="{{$product->image}}" alt="Product Image">
+                <div class="p-4">
+                    <h2 class="text-gray-900 font-bold text-xl">{{$product->title}}</h2>
+                    <p class="mt-2 text-gray-600">{{$product->description}}</p>
+                    <div class="mt-2 flex items-center justify-between">
+                        <span class="text-gray-400 font-bold">Price:</span>
+                        <span class="text-green-500 font-bold text-xl">R${{$product->price}}</span>
+                    </div>
+                    <div class="mt-4">
+                        <button class="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    {{$products->links()}}
 </div>
